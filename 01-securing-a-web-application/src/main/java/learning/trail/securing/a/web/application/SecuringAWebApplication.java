57b834d48bcd862e0e4ef1cc15cfc39fa6a49068
Berk Ekim Pasmakoglu <berk.pasmakoglu@gmail.com>
2024-05-09 11:31:54 +0300

    Add application launcher

---
+++ 01-securing-a-web-application/src/main/java/learning/trail/securing/a/web/application/SecuringAWebApplication.java
@@ -0,0 +1,11 @@
+package learning.trail.securing.a.web.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringAWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecuringAWebApplication.class, args);
+    }
+}
