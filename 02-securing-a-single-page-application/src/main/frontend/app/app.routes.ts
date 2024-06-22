7ec5ba2e31117f8e91555d4feb2fd6e617694cd4
Berk Ekim Pasmakoglu <berkekim@gmail.com>
2024-06-22 16:10:28 +0300

    Add home navigation

--- 02-securing-a-single-page-application/src/main/frontend/app/app.routes.ts
+++ 02-securing-a-single-page-application/src/main/frontend/app/app.routes.ts
@@ -1,3 +1,5 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'}
+];
