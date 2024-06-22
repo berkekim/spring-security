e102f6adcb32091ea6b1310390f3650bfb768aa2
Berk Ekim Pasmakoglu <berkekim@gmail.com>
2024-06-22 16:24:27 +0300

    Revise home component template to add greeting structure

--- 02-securing-a-single-page-application/src/main/frontend/app/home.component.ts
+++ 02-securing-a-single-page-application/src/main/frontend/app/home.component.ts
@@ -1,7 +1,13 @@
 import {Component} from "@angular/core";
 
 @Component({
+  standalone: true,
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  greeting = {} as any;
+
+  authenticated() {
+    return false;
+  }
 }
